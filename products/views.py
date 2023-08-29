59b041438374928666f1525009c35fdab07cf25e
EATataris <emir-ali818@mail.ru>
2023-08-29 08:17:42 +0300

    add dynamic page context from json file

--- products/views.py
+++ products/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    with open('products/fixtures/products.json') as f:
+        data = json.load(f)
+
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': data,
+    }
+    return render(request, 'products/products.html', context)
 
 
