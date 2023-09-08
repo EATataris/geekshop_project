3eb43889f3546a1b443275cffd45f1a7e2f873bc
EATataris <emir-ali818@mail.ru>
2023-09-08 15:39:28 +0300

    add urls for products app and include products urls to a geekshop urls

--- geekshop_project/urls.py
+++ geekshop_project/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 from products.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
 ]
 
 
